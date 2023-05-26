620e78f4a217f5810d25d3d6c564e1b1003456ad
Roy Mbaya <roymbaya08@gmail.com>
2023-05-26 13:10:13 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+-- Insert the bears
+INSERT INTO bears (name, age, sex)
+VALUES
+  ('Mr. Chocolate', 5, 'M'),
+  ('Rowdy', 3, 'M'),
+  ('Tabitha', 2, 'F'),
+  ('Sergeant Brown', 7, 'M'),
+  ('Melissa', 4, 'F'),
+  ('Grinch', 6, 'M'),
+  ('Wendy', 1, 'F'),
+  (NULL, 0, 'F');
